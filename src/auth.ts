321119c660a8b425dcfc841b3bc7222c859ded69
Syed zain ali shah <zaingit780@gmail.com>
2024-09-09 15:04:15 +0500

    add user role in db and modify default session and token and add more details

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import authConfig from "../auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
+import { getUserById } from "@/data/user";
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +11,24 @@ export const {
   signOut,
 } = NextAuth({
   ...authConfig,
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
 });
